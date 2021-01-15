b2ee21da64c0ca8aae284d0f957da398222e923e
Luan <luanalencar134@gmail.com>
2021-01-15 13:18:25 -0300

    Adicionando e implementando classes

--- backend-sge/src/main/java/com/basis/sge/servico/UsuarioCadastroServico.java
+++ backend-sge/src/main/java/com/basis/sge/servico/UsuarioCadastroServico.java
@@ -3,24 +3,26 @@ package com.basis.sge.servico;
 import com.basis.sge.dominio.Usuario;
 import com.basis.sge.exceptions.UserAlreadyExist;
 import com.basis.sge.repositorio.UsuarioRepositorio;
+import com.basis.sge.servico.mapper.UsuarioMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class UsuarioCadastro {
+public class UsuarioCadastroServico {
 
-    private UsuarioRepositorio usuarioRepositorio;
+    private final UsuarioMapper usuarioMapper;
+    private final UsuarioRepositorio usuarioRepositorio;
 
-    public Usuario salvar(Usuario usuario){
+    public Usuario salvar(Usuario usuario) throws UserAlreadyExist {
         Usuario usuarioExiste = usuarioRepositorio.findByEmail(usuario.getEmail());
-        if (usuarioExiste != null && !usuarioExiste.equals(usuario)){
+        if (usuarioExiste != null && !usuarioExiste.equals(usuario)) {
             throw new UserAlreadyExist("Já existe um usário cadastrado com este email!");
         }
         return usuarioRepositorio.save(usuario);
     }
 
-    public void excluir(Integer usuarioId){
+    public void excluir(Integer usuarioId) {
         usuarioRepositorio.deleteById(usuarioId);
     }
 }
