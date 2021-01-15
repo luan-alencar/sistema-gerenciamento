47a3b652fcbde797efc57b0a8437f4e47801397a
Luan <luanalencar134@gmail.com>
2021-01-15 14:12:59 -0300

    Removendo antigas classes Exceptions

--- backend-sge/src/main/java/com/basis/sge/exceptions/UserAlreadyExist.java
+++
@@ -1,8 +0,0 @@
-package com.basis.sge.exceptions;
-
-public class UserAlreadyExist extends RuntimeException {
-
-    public UserAlreadyExist(String s) {
-        super(s);
-    }
-}
--- backend-sge/src/main/java/com/basis/sge/exceptions/UserNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.basis.sge.exceptions;
-
-public class UserNotFoundException extends RuntimeException{
-
-    public UserNotFoundException(String msg){
-        super(msg);
-    }
-}
