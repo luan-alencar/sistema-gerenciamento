7ed018cf42026894b925560b10cd9ca3a5abe4b6
italo santos <neves.italo@gmail.com>
2021-01-15 16:05:02 -0300

    crud pergunta - italo

--- backend-sge/src/main/java/com/basis/sge/servicos/PerguntaServico.java
+++ backend-sge/src/main/java/com/basis/sge/servicos/PerguntaServico.java
@@ -1,7 +1,66 @@
 package com.basis.sge.servicos;
 
 
+import com.basis.sge.dominio.Pergunta;
+import com.basis.sge.repositorio.PerguntaRepositorio;
+import com.basis.sge.servicos.dto.Mapper.PerguntaMapper;
+import com.basis.sge.servicos.dto.PerguntaDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
 public class PerguntaServico {
+
+    private final PerguntaRepositorio perguntaRepositorio;
+    private final PerguntaMapper perguntaMapper;
+
+
+    public List<PerguntaDTO> listar(){
+
+        List<Pergunta> listaPergunta = perguntaRepositorio.findAll(); //Busca no repositório todas as perguntas
+        return perguntaMapper.toDto(listaPergunta); //Passa as perguntas para o dto e envia para os recursos uma listaDto
+
+    }
+
+    public PerguntaDTO buscarPerguntaId(Integer id){
+
+        Pergunta pergunta = perguntaRepositorio.findById(id).get();     //  get () só pode retornar um valor se o objeto empacotado
+        return perguntaMapper.toDto(pergunta);                    // não for nulo, caso contrário, ele lança uma exceção de nenhum elemento:
+
+    }
+
+    public PerguntaDTO salvar (PerguntaDTO perguntaDTO){
+
+        Pergunta pergunta = perguntaMapper.toEntity(perguntaDTO);
+        perguntaRepositorio.save(pergunta);
+
+        return perguntaDTO;
+
+
+    }
+    public  PerguntaDTO atualizar(PerguntaDTO perguntaDTO){
+
+        Pergunta pergunta = perguntaMapper.toEntity(perguntaDTO);
+        perguntaRepositorio.save(pergunta);
+        return perguntaDTO;
+
+    }
+
+    public void deletar (Integer id){
+
+        perguntaRepositorio.deleteById(id);
+
+
+    }
+
+
+
 
 
 
--- backend-sge/src/main/java/com/basis/sge/servicos/dto/PerguntaDTO.java
+++ backend-sge/src/main/java/com/basis/sge/servicos/dto/PerguntaDTO.java
@@ -2,7 +2,18 @@ package com.basis.sge.servicos.dto;
 
 
 //Classe onde fica as regras de negócios
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class PerguntaDTO {
+
+
+    private Integer id;
+
+    private String descricao;
 
 
 }
