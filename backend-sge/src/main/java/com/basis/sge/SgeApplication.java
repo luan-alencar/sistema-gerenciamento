0fe8a99b8c5c0be7f2c12fe4e890e9ef18bbd29e
Luan <luanalencar134@gmail.com>
2021-01-16 14:39:42 -0300

    Preparando classe de servico para adicionar dados ao DB

--- backend-sge/src/main/java/com/basis/sge/SgeApplication.java
+++ backend-sge/src/main/java/com/basis/sge/SgeApplication.java
@@ -1,13 +1,17 @@
 package com.basis.sge;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SgeApplication {
+public class SgeApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SgeApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SgeApplication.class, args);
+    }
 
+    @Override
+    public void run(String... args) throws Exception {
+    }
 }
---
+++ backend-sge/src/main/java/com/basis/sge/config/DevConfig.java
@@ -0,0 +1,14 @@
+package com.basis.sge.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.text.ParseException;
+
+@Service
+@RequiredArgsConstructor
+class DBService {
+
+    public void instantiateDatabase() throws ParseException {
+    }
+}
