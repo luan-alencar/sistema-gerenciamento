83ce33c21bf6dd34b3521a4edf48a4fe227f9f55
Luan <luanalencar134@gmail.com>
2021-01-18 12:32:50 -0300

    Atualizando

--- backend-sge/src/main/java/com/basis/sge/recurso/TipoEventoRecurso.java
+++ backend-sge/src/main/java/com/basis/sge/recurso/TipoEventoRecurso.java
@@ -1,11 +1,19 @@
 package com.basis.sge.recurso;
 
+import com.basis.sge.servico.TipoEventoServico;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/eventos/{tipos}")
+@RequiredArgsConstructor
+@RequestMapping("/api/tiposeventos")
 public class TipoEventoRecurso {
 
+    private final TipoEventoServico tipoEventoServico;
 
+    public ResponseEntity<?> listar(){
+        tipoEventoServico.listar();
+    }
 }
--- backend-sge/src/main/java/com/basis/sge/servico/TipoEventoServico.java
+++ backend-sge/src/main/java/com/basis/sge/servico/TipoEventoServico.java
@@ -18,31 +18,14 @@ public class TipoEventoServico {
     private final TipoEventoRepositorio tipoEventoRepositorio;
     private final TipoEventoMapper tipoEventoMapper;
 
-    @RequestMapping(name = "/api/eventos/tipos")
+    @RequestMapping(name = "/api/tiposeventos")
     public List<TipoEventoDTO> listar() {
         List<TipoEvento> tipoEventoList = tipoEventoRepositorio.findAll();
         return tipoEventoMapper.toDto(tipoEventoList);
     }
 
-    public TipoEventoDTO obterPreInscricaoPorId(Integer id) {
+    public TipoEventoDTO buscar(Integer id) {
         TipoEvento tipoEvento = tipoEventoRepositorio.findById(id).get();
-        return tipoEventoMapper.toDto(tipoEvento);
-    }
-
-    public void deletar(Integer id) {
-        tipoEventoRepositorio.deleteById(id);
-    }
-
-    public TipoEventoDTO atualizar(TipoEventoDTO tipoEventoDTO) {
-        TipoEvento tipoEventoAtualizado = tipoEventoMapper.toEntity(tipoEventoDTO);
-        tipoEventoRepositorio.save(tipoEventoAtualizado);
-        return tipoEventoMapper.toDto(tipoEventoAtualizado);
-    }
-
-    @RequestMapping(name = "/api/eventos/tipos")
-    public TipoEventoDTO salvar(TipoEventoDTO tipoEventoDTO) {
-        TipoEvento tipoEvento = tipoEventoMapper.toEntity(tipoEventoDTO);
-        tipoEventoRepositorio.save(tipoEvento);
         return tipoEventoMapper.toDto(tipoEvento);
     }
 }
