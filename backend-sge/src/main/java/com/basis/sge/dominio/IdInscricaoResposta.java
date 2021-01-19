f6a9170521f6307bfb7b7060dace6f5c4f60d6ad
Luan <luanalencar134@gmail.com>
2021-01-19 12:53:04 -0300

    Atualizando projeto

--- backend-sge/src/main/java/com/basis/sge/dominio/IdInscricaoResposta.java
+++ backend-sge/src/main/java/com/basis/sge/dominio/IdInscricaoResposta.java
@@ -1,6 +1,8 @@
 package com.basis.sge.dominio;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Embeddable;
