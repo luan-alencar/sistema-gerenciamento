d34d994fd7b10986bd9f66f4d5d2c8c7cd7e096c
Luan <luanalencar134@gmail.com>
2021-01-21 00:19:30 -0300

    Implementando classes de Evento para teste - Luan

--- backend-sge/src/test/java/com/basis/sge/builder/EventoBuilder.java
+++ backend-sge/src/test/java/com/basis/sge/builder/EventoBuilder.java
@@ -1,11 +1,23 @@
 package com.basis.sge.builder;
 
+import com.basis.sge.repositorio.EventoRepositorio;
+import com.basis.sge.servico.EventoServico;
 import com.basis.sge.servico.dto.EventoDTO;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.text.ParseException;
 import java.util.List;
 
+@RunWith(SpringRunner.class)
+@Transactional
 public class EventoBuilder extends ConstrutorDeEntidade {
+
+    @Autowired
+    private EventoServico eventoServico;
+
     @Override
     protected EventoDTO construirEntidade() throws ParseException {
         return null;
@@ -18,7 +30,7 @@ public class EventoBuilder extends ConstrutorDeEntidade {
 
     @Override
     protected List<EventoDTO> obterTodos() {
-        return null;
+        return eventoServico.listar();
     }
 
     @Override
---
+++ backend-sge/src/test/java/com/basis/sge/recurso/EventoRecursoIT.java
@@ -0,0 +1,30 @@
+package com.basis.sge.recurso;
+
+import com.basis.sge.builder.EventoBuilder;
+import com.basis.sge.servico.mapper.EventoMapper;
+import com.basis.sge.util.IntTestComum;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@Transactional
+public class EventoRecursoIT extends IntTestComum {
+
+    @Autowired
+    private EventoBuilder eventoBuilder;
+
+    @Autowired
+    private EventoMapper eventoMapper;
+
+    @Test
+    public void listarTest () throws Exception{
+        eventoBuilder.construir();
+        getMockMvc().perform(get("api/eventos"))
+        .andExpect(status().isOk());
+    }
+}
