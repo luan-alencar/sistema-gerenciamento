a2c4fd5680e29cbe295907da1f718733f99524e4
Luan <luanalencar134@gmail.com>
2021-01-22 11:07:09 -0300

    Trocando notacao - Luan

--- backend-sge/src/test/java/com/basis/sge/recurso/EventoRecursoIT.java
+++ backend-sge/src/test/java/com/basis/sge/recurso/EventoRecursoIT.java
@@ -10,8 +10,10 @@ import org.junit.Assert;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
