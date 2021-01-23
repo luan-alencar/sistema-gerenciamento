e0d265d26bec760774e0f64f49e743c8931d96f0
AlineCantalice <alinedeantalice@gmail.com>
2021-01-23 16:20:12 -0300

    criacao das classes que serão necessarias para os testes - aline

---
+++ backend-sge/src/test/java/com/basis/sge/recurso/EventoRecursoIT.java
@@ -0,0 +1,11 @@
+package com.basis.sge.recurso;
+
+import com.basis.sge.util.IntTestComum;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
+
+@ExtendWith(SpringExtension.class)
+@Transactional
+public class EventoRecursoIT extends IntTestComum {
+}
