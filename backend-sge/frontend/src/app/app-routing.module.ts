97ed34e8418b529ad074a2cf06e84f1bb5fc6a8e
AlineCantalice <alinedecantalice@gmail.com>
2021-02-02 14:02:50 -0300

    rotas de evento deletada

--- backend-sge/frontend/src/app/app-routing.module.ts
+++ backend-sge/frontend/src/app/app-routing.module.ts
@@ -1,19 +1,13 @@
-import { LoginComponent } from 'src/app/shared/components/login/login.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginSuccessComponent } from '@nuvem/angular-base';
 import { DiarioErrosComponent } from './components/diario-erros/diario-erros.component';
-import { EventoModule } from './modulos/evento/evento.module';
 import { UsuarioModule } from './modulos/usuario/usuario.module';
 
 const routes: Routes = [
     { 
     path: 'usuarios',
     loadChildren: () => UsuarioModule, 
-    },
-    { 
-      path: 'eventos',
-      loadChildren: () => EventoModule, 
     },
     { path: 'diario-erros', component: DiarioErrosComponent, data: { breadcrumb: 'Diário de Erros'} },
     { path: 'login-success', component: LoginSuccessComponent },
