e4027218c9ebc75de32cd68d33d80bdd47edfdd5
Luan <luanalencar134@gmail.com>
2021-02-07 21:02:34 -0300

    comit

--- backend-sge/frontend/src/app/app.component.ts
+++ backend-sge/frontend/src/app/app.component.ts
@@ -1,8 +1,7 @@
-import { Component, AfterViewInit, ElementRef, Renderer2, ViewChild, OnDestroy, OnInit, NgZone } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, NgZone, OnDestroy, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { MenuOrientation, MenusService } from '@nuvem/primeng-components';
 import { ScrollPanel } from 'primeng';
-import { MenusService, MenuOrientation } from '@nuvem/primeng-components';
-import { RouterLink } from '@angular/router';
-import { Usuario } from 'src/app/dominios/usuario';
+import { Usuario } from './dominios/usuario';
 import { LoginComponent } from './shared/components/login/login.component';
 
 @Component({
